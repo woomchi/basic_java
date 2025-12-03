ad92803700accdfbc98a1ef7f41a65763656df59
woomchi <hello.git.hitszombie@gmail.com>
2025-12-03 10:20:23 +0900

    251202

---
+++ Chapter2/src/Section1/MyPoint1.java
@@ -0,0 +1,8 @@
+package Section1;
+
+public class MyPoint1 {
+
+	public int x;
+	public int y;
+	
+}
---
+++ Chapter2/src/Section1/MyRectangle.java
@@ -0,0 +1,9 @@
+package Section1;
+
+public class MyRectangle {
+	
+	public MyPoint1 lu;
+	public int width;
+	public int height;
+	
+}
